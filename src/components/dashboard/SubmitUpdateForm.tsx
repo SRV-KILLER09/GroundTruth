c83a91ffe04eadeea99ed2f4b10199003a2eb5ab
Saransh Wadhwa <saranshwadhwa0102@gmail.com>
2025-09-11 12:32:26 +0000

    also add an option for user to enter the coordinates manually.

--- src/components/dashboard/SubmitUpdateForm.tsx
+++ src/components/dashboard/SubmitUpdateForm.tsx
@@ -369,7 +369,7 @@ export function SubmitUpdateForm({ onSuccessfulSubmit }: SubmitUpdateFormProps) 
                 )}
                 Acquire My Location
             </Button>
-            <div className="grid grid-cols-2 gap-4 hidden">
+            <div className="grid grid-cols-2 gap-4">
                 <FormField
                 control={form.control}
                 name="latitude"
